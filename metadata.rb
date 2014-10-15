e17ef8d79381080097d86bd0d0306c1cbda66325
Sebastian Trahm <basti@schleifi.com>
2014-10-15 07:44:16 +0200

    platform dependent modification of group "dialup"
    * Add group helper providing platform check for vagrant host
      so we can conditionally adapt the group "dialup"
    * Adapt dependencies and cleanup

--- metadata.rb
+++ metadata.rb
@@ -1,5 +1,4 @@
 name             "avr"
-
 maintainer       "Sebastian Trahm"
 maintainer_email "sebastian.trahm@gmail.com"
 license          "Apache 2.0"
@@ -7,8 +6,6 @@ description      "Installs AVR-GCC Toolchain"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.1.0"
 
-recommends 'git'
-
+depends 'apt'
 supports 'ubuntu'
-
 provides 'avr'
--- recipes/default.rb
+++ recipes/default.rb
@@ -1,3 +1,11 @@
 node['avr']['packages'].each do |pkg|
     package pkg
 end
+
+Chef::Resource::Group.send(:include, Group::Helper)
+group "dialout" do
+  action :modify
+  members "vagrant"
+  append true
+  only_if { on_vagrant? }
+end
