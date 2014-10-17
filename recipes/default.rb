2cbaefab9e348e823991cdb2c769c2fff2e81e8e
Sebastian Trahm <basti@schleifi.com>
2014-10-17 13:59:09 +0200

    Move group helper to specific recipe
    Move the group helper coping with setting group
    "dialout" on Vagrant hosts to a specific recipe to
    be included only if explicitly adding "avr::vagrant"
    to the list of recipes.

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,11 +1,3 @@
 node['avr']['packages'].each do |pkg|
     package pkg
 end
-
-Chef::Resource::Group.send(:include, Group::Helper)
-group "dialout" do
-  action :modify
-  members "vagrant"
-  append true
-  only_if { on_vagrant? }
-end
